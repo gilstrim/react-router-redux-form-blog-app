01eb530d299c40a5b18b22e60e29d873f26751f4
Freddy <freddyjones236@gmail.com>
2018-08-16 05:27:58 +0200

    Added redux-form as well as the Switch from react-router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer // needs to be assigned to form piece of state
 });
 
 export default rootReducer;
